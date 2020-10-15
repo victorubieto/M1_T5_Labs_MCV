3ef555a71a9ccea08787ebfdd65b04533ec74934
atikiwiki5 <moha.atiki@gmail.com>
2020-10-15 18:51:41 +0200

    MAPk calculation for different type of distances and window size

--- M1_T1_cleanUp.py
+++ M1_T1_cleanUp.py
@@ -6,10 +6,16 @@ Created on Wed Oct 14 13:11:36 2020
 """
 import cv2 as cv
 import glob
+import pickle
 from matplotlib import pyplot as plt
 import numpy 
 import heapq
 from scipy.spatial import distance
+
+from scipy.stats import chisquare
+
+import dictances as ds
+import ml_metrics as metrics
 
 
 def getImagesPaths(path,extension):
@@ -60,9 +66,104 @@ def getHistogram(imgArray, keyWord):
             histogramArray.append(h_gNormalized) 
     return histogramArray
 
+
+def alignHistograms(reference, sample, numberOfPoints):
+    
+    tiny = 1e-15
+    
+    #get the max position of each histogram
+    referenceHistogramMaxPositions = []
+    sampleHistogramMaxPositions = []
+    
+    referenceHistogramWithWindow = []
+    sampleHistogramWithWindow = []
+    
+    referenceHistogramWithWindowPlusTiny = []
+    sampleHistogramWithWindowPlusTiny = []
+    
+    for step in reference:
+        referenceHistogramMaxPositions.append(int(numpy.where(step == max(step))[0][0]))
+    
+    for step in sample:
+        sampleHistogramMaxPositions.append(int(numpy.where(step == max(step))[0][0]))
+
+    #Apply a rectangular window centred into the max position wit bandwith of numberOfPoints
+    for index in range(0,len(reference)):
+        step = reference[index]
+        maxPosition = referenceHistogramMaxPositions[index]
+        # print('INDEX ' + str(maxPosition))
+        if (maxPosition - numberOfPoints) < 0:
+            # print('A')
+            referenceHistogramWithWindow.append(step[0:2*numberOfPoints])
+            # print('AA')
+        elif (maxPosition + numberOfPoints) > 255:
+            # print('B')
+            referenceHistogramWithWindow.append(step[255 - 2*numberOfPoints:255])
+            # print('BB')
+        
+        else:
+            # print('C')
+            referenceHistogramWithWindow.append(step[maxPosition - numberOfPoints:maxPosition + numberOfPoints])
+            # print('CC')
+
+        
+    for index in range(0,len(sample)):
+        step = sample[index]
+        maxPosition = sampleHistogramMaxPositions[index]
+        # print('INDEX SAMPLE ' + str(maxPosition))
+        if (maxPosition - numberOfPoints) < 0:
+            # print('A')
+            sampleHistogramWithWindow.append(step[0:2*numberOfPoints])
+            # print('AA')
+        
+        elif (maxPosition + numberOfPoints) > 255:
+            # print('B')
+            sampleHistogramWithWindow.append(step[255 - 2*numberOfPoints:255])
+            # print('BB')
+        
+        else:
+            # print('C')
+            sampleHistogramWithWindow.append(step[maxPosition - numberOfPoints:maxPosition + numberOfPoints])
+            # print('CC')
+            
+        # if index == 28:
+            # print('POSICION 28: ' + str(step[maxPosition - numberOfPoints:maxPosition + numberOfPoints]))
+            # print('MAXPOSICION 28: ' + str(maxPosition))
+            # print('NUMBEROFPOINTS 28: ' + str(numberOfPoints))
+    # print('LONGITUD = '+str(referenceHistogramWithWindow))
+    #Add a little offset to delete the zero value
+    for pos in range(0,len(referenceHistogramWithWindow)):
+        
+        # print('CACA ->' + str(referenceHistogramWithWindow[pos]))
+        referenceHistogramWithWindowPlusTiny.append([])
+        # print('RR')
+        step = referenceHistogramWithWindow[pos]
+        for index in range(0,len(step)):
+            referenceHistogramWithWindowPlusTiny[pos].append(float(referenceHistogramWithWindow[pos][index] + tiny))
+        
+    
+    for pos in range(0,len(sampleHistogramWithWindow)):
+        sampleHistogramWithWindowPlusTiny.append([])
+        step = sampleHistogramWithWindow[pos]
+        for index in range(0,len(step)):
+            sampleHistogramWithWindowPlusTiny[pos].append(float(sampleHistogramWithWindow[pos][index] + tiny))
+        
+        
+    # print('MAX POSITION REF: ' + str(max(referenceHistogramMaxPositions)))  
+    # print('MIN POSITION REF: ' + str(min(referenceHistogramMaxPositions)))
+    
+    # print('MAX POSITION SAMPLE: ' + str(max(sampleHistogramMaxPositions)))  
+    # print('MIN POSITION SAMPLE: ' + str(min(sampleHistogramMaxPositions)))
+    
+    
+    return referenceHistogramWithWindowPlusTiny,sampleHistogramWithWindowPlusTiny
+
+    
+
 def getMetrics(metricName, reference, sample):
     metric = []
     bestResults = []
+    metricValues = []
     
     for i in range(0,len(sample)):
         sampleImageHistogram = sample[i]
@@ -89,13 +190,134 @@ def getMetrics(metricName, reference, sample):
             elif metricName == 'hellinger':
                 metric[i].append(cv.compareHist(sampleImageHistogram, referenceImageHistogram, cv.HISTCMP_HELLINGER))
             
+    
+    
+    
+    if metricName == 'testingCorrelation':
+        metric = []
+        for sampleHistIndex in range(0,len(sample)):
+            metric.append([])
             
+            sampleHist = sample[sampleHistIndex]
+            
+            for referenceHistIndex in range(0, len(reference)):
+
+                referenceHist = reference[referenceHistIndex]
+
+                corrSampleReference = numpy.corrcoef(sampleHist,referenceHist)[0][1]
+                metric[sampleHistIndex].append(corrSampleReference)
+    
+    if metricName == 'testingChiSquare':
+        metric = []
+        for sampleHistIndex in range(0,len(sample)):
+            metric.append([])
+            sampleHist = sample[sampleHistIndex]
+            
+            for referenceHistIndex in range(0, len(reference)):
+                referenceHist = reference[referenceHistIndex]
+                chiSquareSampleReference = chisquare(sampleHist,referenceHist)[1]    
+                metric[sampleHistIndex].append(chiSquareSampleReference)
+   
+    if metricName == 'testingEuclideanDistance':
+        metric = []
+        for sampleHistIndex in range(0,len(sample)):
+            metric.append([])
+            sampleHist = sample[sampleHistIndex]
+            
+            for referenceHistIndex in range(0, len(reference)):
+                referenceHist = reference[referenceHistIndex]
+                euclideanDist = distance.euclidean(sampleHist,referenceHist)
+                metric[sampleHistIndex].append(euclideanDist)
+    
+    # if metricName == 'testingBhattacharyya':
+    #     metric = []
+    #     for sampleHistIndex in range(0,len(sample)):
+    #         metric.append([])
+    #         sampleHist = sample[sampleHistIndex]
+            
+    #         for referenceHistIndex in range(0, len(reference)):
+    #             referenceHist = reference[referenceHistIndex]
+                
+    #             ds.bhattacharyya(a, b)
+    
+
+    
+        
     for metric_n in metric:
         bestResults.append(heapq.nlargest(10, range(len(metric_n)), metric_n.__getitem__))
-    return bestResults
+    
+    for index in range(0,len(bestResults)):
+        metricValues.append([])
+        bestResultsOfThisIteration = bestResults[index]
+        for imageIndex in bestResultsOfThisIteration:
+            metricValues[index].append(metric[index][imageIndex])
+    
+    return bestResults,metricValues
+
+def getResults(metrics,keyWord):
+    global data
+    with open(r'C:\Users\mohaa\OneDrive\Escritorio\masterCV\1_M1\1_T1\material\1_images\qsd1_w1\qsd1_w1\gt_corresps.pkl', 'rb') as f:
+        data = pickle.load(f)
+    
+    detected = 0
+    
+    # print('**************************************')
+    # print('Images detected with ' + str(keyWord) + ' metric')
+    for i in range(0,len(metrics)):
+        if data[i][0] in metrics[i]:
+            detected = detected + 1
+            # print('    -> Image number ' +str(i) + ' detected between the 10 best values')
+            
+    # print('    TOTAL IMAGES DETECTED = ' + str(detected) + "\n\n\n")
+    # print('**************************************')
+    return detected
+
+def getOptimiziedMetricForHistogramsWithWindow(reference,sample):
+    global windowSize,correlationMetricsTestingIDs,chiSquareMetricsTestingIDs,euclideanDistanceMetricsTestingIDs,correlationMetricsTestingValues,chiSquareMetricsTestingValues,euclideanDistanceMetricsTestingValues
+    
+    windowSize = []
+    chiSquareDetectedImages = []
+    correlationDetectedImages = []
+    euclideanDistanceDetectedImages = []
+    
+    
+    mAPkChiSquareWindow = []
+    mAPkCorrelationWindow = []
+    mAPkEuclideanDistanceWindow = []
+    
+    #size in samples
+    minWindowSize = 1
+    maxWindowSize = 128
+    
+    for windowSamples in range(minWindowSize,maxWindowSize):
+        #align histograms test
+        print('Step ' + str(windowSamples))
+        referenceHistogramWithWindoww,sampleHistogramWithWindoww = alignHistograms(reference, sample, windowSamples)
+        
+        #get testing metrics
+        correlationMetricsTestingIDs,correlationMetricsTestingValues = getMetrics('testingCorrelation', referenceHistogramWithWindoww, sampleHistogramWithWindoww)
+        chiSquareMetricsTestingIDs,chiSquareMetricsTestingValues = getMetrics('testingChiSquare', referenceHistogramWithWindoww, sampleHistogramWithWindoww)
+        euclideanDistanceMetricsTestingIDs,euclideanDistanceMetricsTestingValues = getMetrics('testingEuclideanDistance', referenceHistogramWithWindoww, sampleHistogramWithWindoww)
+        
+        windowSize.append(windowSamples)
+        chiSquareDetectedImages.append(getResults(correlationMetricsTestingIDs,'testingChiSquare'))
+        correlationDetectedImages.append(getResults(chiSquareMetricsTestingIDs,'testingCorrelation'))
+        euclideanDistanceDetectedImages.append(getResults(euclideanDistanceMetricsTestingIDs,'euclideanDistance'))
+        
+        mAPkChiSquareWindow.append(getMAPk(chiSquareMetricsTestingIDs,10))
+        mAPkCorrelationWindow.append(getMAPk(correlationMetricsTestingIDs,10))
+        mAPkEuclideanDistanceWindow.append(getMAPk(euclideanDistanceMetricsTestingIDs,10))
+        
+    return windowSize,chiSquareDetectedImages,correlationDetectedImages,euclideanDistanceDetectedImages,mAPkChiSquareWindow,mAPkCorrelationWindow,mAPkEuclideanDistanceWindow
+
+def getMAPk(query,k):
+    global data
+    mAPk =  metrics.mapk(data, query ,k)
+    return mAPk
 
 def main():
-    global correlationMetrics, chiSquareMetrics, histogramIntersectionMetrics, bhattacharyyaMetrics,hellingerMetrics, euclideanDistanceMetrics
+    global correlationMetrics, sampleGrayImagesHistogram, chiSquareMetrics, histogramIntersectionMetrics, bhattacharyyaMetrics,hellingerMetrics, euclideanDistanceMetrics, jpgGrayImagesHistogram, referenceHistogramWithWindoww,sampleHistogramWithWindoww,correlationMetricsTesting,chiSquareMetricsTesting,histogramIntersectionMetricsTesting,bhattacharyyaMetricsTesting,euclideanDistanceMetricsTesting,hellingerMetricsTesting
+
     
     #define dif paths
     pathDB = r'C:\Users\mohaa\OneDrive\Escritorio\masterCV\1_M1\1_T1\material\1_images\BBDD\BBDD'
@@ -121,13 +343,77 @@ def main():
     pngRGBImagesHistogram = getHistogram(jpgRGBImages, 'RGB')
     sampleRGBImagesHistogram = getHistogram(sampleRGBImages, 'RGB')
     
-    #get metrics
-    correlationMetrics = getMetrics('correlation', jpgGrayImagesHistogram, sampleGrayImagesHistogram)
-    chiSquareMetrics = getMetrics('chiSquare', jpgGrayImagesHistogram, sampleGrayImagesHistogram)
-    histogramIntersectionMetrics = getMetrics('histogramIntersection', jpgGrayImagesHistogram, sampleGrayImagesHistogram)
-    bhattacharyyaMetrics = getMetrics('bhattacharyya', jpgGrayImagesHistogram, sampleGrayImagesHistogram)
-    euclideanDistanceMetrics = getMetrics('euclideanDistance', jpgGrayImagesHistogram, sampleGrayImagesHistogram)
-    hellingerMetrics = getMetrics('hellinger', jpgGrayImagesHistogram, sampleGrayImagesHistogram)
+    #get metrics of the histograms without windowing
+    correlationMetricsIDs, correlationMetricsValues = getMetrics('correlation', jpgGrayImagesHistogram, sampleGrayImagesHistogram)
+    chiSquareMetricsIDs, chiSquareMetricsValues = getMetrics('chiSquare', jpgGrayImagesHistogram, sampleGrayImagesHistogram)
+    histogramIntersectionMetricsIDs, histogramIntersectionMetricsValues = getMetrics('histogramIntersection', jpgGrayImagesHistogram, sampleGrayImagesHistogram)
+    bhattacharyyaMetricsIDs,bhattacharyyaMetricsValues = getMetrics('bhattacharyya', jpgGrayImagesHistogram, sampleGrayImagesHistogram)
+    euclideanDistanceMetricsIDs,euclideanDistanceMetricsValues = getMetrics('euclideanDistance', jpgGrayImagesHistogram, sampleGrayImagesHistogram)
+    hellingerMetricsIDs, hellingerMetricsValues = getMetrics('hellinger', jpgGrayImagesHistogram, sampleGrayImagesHistogram)
+ 
+    
+    # getResults(correlationMetrics,'correlation')
+    # getResults(chiSquareMetrics,'chiSquare')
+    # getResults(histogramIntersectionMetrics,'histogramIntersection')
+    # getResults(bhattacharyyaMetrics,'bhattacharyya')
+    # getResults(euclideanDistanceMetrics,'euclideanDistance')
+    # getResults(hellingerMetrics,'hellinger') 
+    
+
+    global xAxe,chiSquareAxe,correlationAxe,euDistAxe,mAPkChiSquareWindow,mAPkCorrelationWindow,mAPkEuclideanDistanceWindow
+    #get the best metric with the best window size for our gray level histograms
+    xAxe,chiSquareAxe,correlationAxe,euDistAxe,mAPkChiSquareWindow,mAPkCorrelationWindow,mAPkEuclideanDistanceWindow = getOptimiziedMetricForHistogramsWithWindow(jpgGrayImagesHistogram,sampleGrayImagesHistogram)
+    
+    global figure1, figure2
+    
+    figure1 = plt.figure(1)
+    
+    plt.plot(xAxe, euDistAxe , color = 'b', label = 'euclideanDistanceWithWindow')
+    plt.plot(xAxe, correlationAxe, color = 'r', label = 'correlationWithWindow')
+    plt.plot(xAxe, chiSquareAxe, color = 'g', label = 'chiSquareWithWindow')
+    plt.legend(loc=4)
+    
+    figure2 = plt.figure(2)
+    plt.plot(xAxe, mAPkEuclideanDistanceWindow , color = 'b', label = 'euclideanDistanceMAPk')
+    plt.plot(xAxe, mAPkCorrelationWindow, color = 'r', label = 'correlationWithMAPk')
+    plt.plot(xAxe, mAPkChiSquareWindow, color = 'g', label = 'chiSquareWithMAPk')
+    plt.legend(loc=4)
+    
+    
+    # for windowSamples in range(3,128):
+    # #align histograms test
+    #     print('Step ' + str(windowSamples))
+    #     referenceHistogramWithWindoww,sampleHistogramWithWindoww = alignHistograms(jpgGrayImagesHistogram, sampleGrayImagesHistogram, windowSamples)
+    
+    #     #get testing metrics
+    #     correlationMetricsTesting = getMetrics('testingCorrelation', referenceHistogramWithWindoww, sampleHistogramWithWindoww)
+    #     chiSquareMetricsTesting = getMetrics('testingChiSquare', referenceHistogramWithWindoww, sampleHistogramWithWindoww)
+        
+    #     # getResults(correlationMetricsTesting,'testingCorrelation')
+    #     # getResults(chiSquareMetricsTesting,'testingChiSquare')
+    
+    #     windowSize.append(windowSamples)
+    #     chiSquareDetectedImages.append(getResults(chiSquareMetricsTesting,'testingChiSquare'))
+    #     correlationDetectedImages.append(getResults(correlationMetricsTesting,'testingCorrelation'))
+    
+
+    
+    
+    
+    # plt.subplot(4,1,1)
+    # plt.plot(referenceHistogramWithWindoww[120],'Gray')
+    # plt.subplot(4,1,1)
+    # plt.plot(sampleHistogramWithWindoww[0],'Red')
+    
+    # plt.subplot(4,1,2)
+    # plt.plot(referenceHistogramWithWindoww[170],'Gray')
+    # plt.subplot(4,1,2)
+    # plt.plot(sampleHistogramWithWindoww[1],'Red')
+    
+    # plt.subplot(4,1,3)
+    # plt.plot(referenceHistogramWithWindoww[277],'Gray')
+    # plt.subplot(4,1,3)
+    # plt.plot(sampleHistogramWithWindoww[2],'Red')
     
     #plot first image
     # plt.subplot(4,1,1)
@@ -172,24 +458,24 @@ def main():
 
 
     # #plot third image
-    plt.subplot(4,1,1)
-    plt.plot(jpgGrayImagesHistogram[277],'Gray')
-    plt.subplot(4,1,2)
-    plt.plot(jpgRGBImagesHistogram[277][0],'Blue')
-    plt.subplot(4,1,3)
-    plt.plot(jpgRGBImagesHistogram[277][1],'Green')
-    plt.subplot(4,1,4)
-    plt.plot(jpgRGBImagesHistogram[277][2],'Red')
-    plt.subplot(4,1,3)
+    # plt.subplot(4,1,1)
+    # plt.plot(jpgGrayImagesHistogram[277],'Gray')
+    # plt.subplot(4,1,2)
+    # plt.plot(jpgRGBImagesHistogram[277][0],'Blue')
+    # plt.subplot(4,1,3)
+    # plt.plot(jpgRGBImagesHistogram[277][1],'Green')
+    # plt.subplot(4,1,4)
+    # plt.plot(jpgRGBImagesHistogram[277][2],'Red')
+    # plt.subplot(4,1,3)
     
-    plt.subplot(4,1,1)
-    plt.plot(sampleGrayImagesHistogram[2],'k')
-    plt.subplot(4,1,2)
-    plt.plot(sampleRGBImagesHistogram[2][0],'c')
-    plt.subplot(4,1,3)
-    plt.plot(sampleRGBImagesHistogram[2][1],'y')
-    plt.subplot(4,1,4)
-    plt.plot(sampleRGBImagesHistogram[2][2],'m')    
+    # plt.subplot(4,1,1)
+    # plt.plot(sampleGrayImagesHistogram[2],'k')
+    # plt.subplot(4,1,2)
+    # plt.plot(sampleRGBImagesHistogram[2][0],'c')
+    # plt.subplot(4,1,3)
+    # plt.plot(sampleRGBImagesHistogram[2][1],'y')
+    # plt.subplot(4,1,4)
+    # plt.plot(sampleRGBImagesHistogram[2][2],'m')    
     
     
     
